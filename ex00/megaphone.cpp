0866c21066d0eb24067ddbaa703fec70f77dafb2
Muhammad Afzal <mafzal@c3r11s3.42warsaw.pl>
2026-06-14 00:34:47 +0200

    new Code:

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafzal < mafzal@student.42warsaw.pl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/13 20:01:50 by mafzal            #+#    #+#             */
+/*   Updated: 2026/06/13 20:46:05 by mafzal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cctype>
+#include <iostream>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		std::string str(argv[i]);
+		for (size_t j = 0; j < str.size(); j++)
+			str[j] = std::toupper((str[j]));
+		std::cout << str;
+		if (i + 1 < argc)
+			std::cout << " ";
+	}
+	std::cout << "\n";
+	return (0);
+}
